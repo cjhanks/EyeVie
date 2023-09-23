0861d7407885bab06d16e32b470b90e6c336cb75
Zeek Oni <web@cjhanks.name>
2023-09-22 18:39:49 -0700

    Initialize repo with existing code.

---
+++ src/VoxelGridMap.hpp
@@ -0,0 +1,4 @@
+#ifndef VOXEL_GRID_MAP_HPP_
+#define VOXEL_GRID_MAP_HPP_
+
+#endif
---
+++ src/detail/SuperGrid.hpp
@@ -0,0 +1,11 @@
+#ifndef DETAIL_SUPER_GRID_HPP_
+#define DETAIL_SUPER_GRID_HPP_
+
+namespace IV { namespace detail {
+struct EmptyDataStruct {};
+
+} // ns detail
+} // ns IV
+
+
+#endif // DETAIL_SUPER_GRID_HPP_
